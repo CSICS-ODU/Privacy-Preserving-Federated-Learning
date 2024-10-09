e590e88d4b182e2d03ca56513c22cedea1b15b32
Aayush Kapoor <83492835+aayush-kapoor@users.noreply.github.com>
2024-10-09 12:19:12 -0400

    Update script_run.sh

--- script_run.sh
+++ script_run.sh
@@ -2,7 +2,7 @@
 
 #SBATCH -c 8
 #SBATCH -p gpu --gres=gpu:1
-#SBATCH -o new-Cifar10-2-%j.out
+#SBATCH -o new-Cifar100-2-%j.out
 module load container_env pytorch-gpu
 
-crun -p ~/envs/new_nvflare python3 fedavg_script_runner_pt.py -n 2 -r 2 -d CIFAR10
+crun -p ~/envs/new_nvflare python3 fedavg_script_runner_pt.py -n 2 -r 2 -d CIFAR100
