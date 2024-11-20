754c7f6f8e9d356ff79eb17bcba4e78f7117f471
Aayush Kapoor <83492835+aayush-kapoor@users.noreply.github.com>
2024-11-20 14:13:17 -0500

    Update pt_fl.py

--- code/pt_fl.py
+++ code/pt_fl.py
@@ -15,10 +15,12 @@
 import os
 import argparse
 import torch
+import pdb, traceback
 
-from code.utilities.models import load_model_defination
-from code.utilities.datasets import load_partitioned_datasets
-from code.simple_network import SimpleNetwork
+from src.utilities.models import load_model_defination
+from src.utilities.datasets import load_partitioned_datasets, get_dataloaders_subset
+from src.utilities.training_utils import save_model, wandb_init,  print_info, get_device, train, test, load_model as load_saved_weights
+from src.simple_network import SimpleNetwork
 
 from torch import nn
 from torch.optim import Adam
@@ -50,6 +52,7 @@ def main():
     [trainloader, valloaders, testloader, _ ], num_channels, num_classes = load_partitioned_datasets(num_clients=n_clients, dataset_name=dataset_name, data_path=data_path, batch_size=32,split=None) 
     
     print("******** MODEL NAME: ", model_name)
+    print("******** DATASET NAME: ", dataset_name)
     print("******** num channels: ", num_channels)
     print("******** num classes: ", num_classes)
 
@@ -57,8 +60,8 @@ def main():
 
 
     batch_size = 4
-    epochs = 5
-    lr = 0.01
+    epochs = 100
+    lr = 0.001
     # model = SimpleNetwork()
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     loss = nn.CrossEntropyLoss() # Same as criterion
@@ -74,15 +77,17 @@ def main():
     sys_info = flare.system_info()
     client_name = sys_info["site_name"]
 
-    train_dataset = CIFAR10(
-        root=os.path.join(DATASET_PATH, client_name), transform=transforms, download=True, train=True
-    )
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    # train_dataset = CIFAR10(
+    #     root=os.path.join(DATASET_PATH, client_name), transform=transforms, download=True, train=True
+    # )
+    # train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 
     summary_writer = SummaryWriter()
     while flare.is_running():
         input_model = flare.receive()
         print(f"current_round={input_model.current_round}")
+        print(f"input model={input_model.meta}")
+
 
         model.load_state_dict(input_model.params)
         model.to(device)
@@ -91,7 +96,7 @@ def main():
         for epoch in range(epochs):
             running_loss = 0.0
             print("**********************************")
-            print("NVIDIA TL", train_loader)
+            # print("NVIDIA TL", train_loader)
             print("MY TL", trainloader[0])
             print("**********************************")
             for i, batch in enumerate(trainloader[0]):
@@ -112,7 +117,7 @@ def main():
 
         print("Finished Training")
 
-        PATH = f"/home/akapo004/new_nvflare/saved_models/{dataset_name}_net.pth"
+        PATH = f"/home/akapo004/new_nvflare/saved_models/{dataset_name}_{n_clients}_net.pth.tar"
         torch.save(model.state_dict(), PATH)
 
         output_model = flare.FLModel(
@@ -121,6 +126,78 @@ def main():
         )
 
         flare.send(output_model)
+
+        
+        evaluate(evaluation_model=f"{dataset_name}_{n_clients}_net", device=device, dataset_name=dataset_name, model_name=model_name, n_clients = n_clients , data_path= data_path)
+
+def evaluate(evaluation_model, device, dataset_name, model_name, n_clients, data_path):
+    
+    print_info(device, model_name, dataset_name)    
+    try:
+        
+        print("************ NOW RUNNING EVALUATE **************")
+        [train_loaders, val_loaders, test_loader, _], num_channels, num_classes = load_partitioned_datasets(num_clients=n_clients, dataset_name=dataset_name, data_path=data_path, batch_size=32,split=None) 
+        
+        val_loader = val_loaders[0]   
+        train_loader = train_loaders[0]
+
+        test_loader_size = len(test_loader.dataset)
+
+
+        train_loader = get_dataloaders_subset(train_loader, test_loader_size)
+        
+        model = load_model_defination(model_name=model_name, num_channels=num_channels, num_classes=num_classes).to(device) 
+        # model = load_model_defination(model_name, num_channels, num_classes)
+        optimizer = torch.optim.Adam(model.parameters())
+
+        print("************ model and dataset loaded perfectly in evaluate function **************")
+        # model, optimizer, train_loader = make_private(differential_privacy, model, optimizer, train_loader)
+
+
+        load_saved_weights(model, filename = evaluation_model)
+
+        print("************ weights loaded perfectly **************")
+
+        comment = 'Test_Centralized_('+evaluation_model+')_'+model_name+'_'+dataset_name
+        # if wandb_logging:
+        #     wandb_init(comment=comment, model_name=model_name, dataset_name=dataset_name)
+        #     wandb.watch(model, log_freq=100)
+            
+        trn_loss, trn_accuracy, predA = test(model=model, loader=train_loader, device=device)
+        val_loss, val_accuracy, _ = test(model=model, loader=val_loader, device=device)
+        tst_loss, tst_accuracy, predB = test(model=model, loader=test_loader, device=device)
+
+
+        
+        # pdb.set_trace()
+
+
+
+
+        print(f"Final training set performance:\n\tloss {trn_loss}\n\taccuracy {trn_accuracy}")
+
+
+
+
+
+
+
+
+        # if wandb_logging:
+        #     wandb.log({"train_acc": trn_accuracy, "train_loss": trn_loss})
+        #     wandb.log({"acc": val_accuracy, "loss": val_loss}, step = 100)
+        #     wandb.log({"test_acc": tst_accuracy, "test_loss": tst_loss})
+        #     wandb.finish()
+        print(f"Final validation set performance:\n\tloss {val_loss}\n\taccuracy {val_accuracy}")
+        print(f"Final test set performance:\n\tloss {tst_loss}\n\taccuracy {tst_accuracy}")
+
+        # plot_histogram(predA, predB)
+            
+        # if wandb_logging:
+        #     wandb.finish()
+    except Exception as e:
+        traceback.print_exc()
+        # pdb.set_trace()
 
 
 if __name__ == "__main__":
