ee3d4b0a827dc09f1dc1630877ec2cc982b98298
Aayush Kapoor <83492835+aayush-kapoor@users.noreply.github.com>
2024-12-01 19:07:29 -0500

    Update fedavg_script_runner_pt.py

--- fedavg_script_runner_pt.py
+++ fedavg_script_runner_pt.py
@@ -1,16 +1,3 @@
-# Copyright (c) 2024, NVIDIA CORPORATION.  All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 import argparse
 from src.utilities.models import load_model_defination
 from src.utilities.datasets import load_partitioned_datasets
@@ -20,18 +7,19 @@ from nvflare.app_opt.pt.job_config.fed_avg import FedAvgJob
 from nvflare.job_config.script_runner import ScriptRunner
 
 if __name__ == "__main__":
-    
     parser = argparse.ArgumentParser(description='A description of your program')
     parser.add_argument('-r', '--num_rounds', type=int, default=2, help='Number of rounds')
     parser.add_argument('-n', '--num_clients', type=int, default=2, help='Number of clients')
     parser.add_argument('-d', '--dataset_name', type=str, default='CIFAR10', help='Dataset name')
     parser.add_argument('-m', '--model_name', type=str, default='efficientnet', help='Model name')
+    parser.add_argument('-a', '--aggregation_function', type=str, default='fedavg', help='Aggregation function')    
     args = parser.parse_args()
 
     n_clients = args.num_clients
     num_rounds = args.num_rounds
     dataset_name = args.dataset_name
     model_name = args.model_name
+    aggregation_function = args.aggregation_function
     data_path = "~/dataset"
 
     [trainloader, valloaders, testloader, _ ], num_channels, num_classes = load_partitioned_datasets(num_clients=n_clients, dataset_name=dataset_name, 
@@ -51,7 +39,7 @@ if __name__ == "__main__":
     # Add clients
     for i in range(n_clients):
         executor = ScriptRunner(
-            script=train_script, script_args=f"--num_rounds {num_rounds} --num_clients {n_clients} --dataset_name {dataset_name} --model_name {model_name}"  # f"--batch_size 32 --data_path /tmp/data/site-{i}"
+            script=train_script, script_args=f"--num_rounds {num_rounds} --num_clients {n_clients} --dataset_name {dataset_name} --model_name {model_name} --aggregation_function {aggregation_function}"  # f"--batch_size 32 --data_path /tmp/data/site-{i}"
         )
         job.to(executor, f"site-{i + 1}")
 
